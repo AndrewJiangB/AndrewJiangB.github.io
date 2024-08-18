208c73ce41e619f2a214879c8d89018fc5808180
mangoicecream <andrew.jiang.b@gmail.com>
2024-08-18 09:17:18 -0400

    life

---
+++ life_components/life_game.js
@@ -0,0 +1,102 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+class Box {
+  constructor(x, y, component, type = "ground") {
+    this.x = x;
+    this.y = y;
+    this.className = type;
+    this.component = component;
+    this.component.className = this.className;
+    this.component.onclick = () => {
+      if (this.className == "ground") {
+        this.className = "life";
+        this.component.className = "life";
+      } else if (this.className == "life") {
+        this.className = "ground";
+        this.component.className = "ground";
+      }
+    };
+  }
+
+  render() {
+    return this.component;
+  }
+}
+
+export default class LifeGame {
+  constructor() {
+    this.width = 10;
+    this.length = 10;
+    this.box_list = [];
+  }
+
+  setup() {
+    var div_el;
+    var div_row;
+    var game_field = document.getElementById("game_field");
+
+    for (var l = 0; l < this.length; l++) {
+      let box_row = [];
+      //Generate Row
+      div_row = document.createElement("div");
+      div_row.className = "row";
+      for (var w = 0; w < this.width; w++) {
+        //Generate column
+        div_el = document.createElement("div");
+        div_el.className = "ground";
+
+        // Generate Container Object
+        let box = new Box(l, w, div_el);
+        box_row.push(box);
+
+        //Add to row node
+        div_row.appendChild(div_el);
+      }
+      this.box_list.push(box_row);
+      game_field.appendChild(div_row);
+    }
+  }
+
+  start() {
+    let next_state = [];
+    this.box_list.forEach((row, l) => {
+      let next_row = [];
+      row.forEach((box, w) => {
+        let count = 0;
+        let neighbors = [
+          [l - 1, w - 1],
+          [l - 1, w],
+          [l - 1, w + 1],
+          [l, w - 1],
+          [l, w + 1],
+          [l + 1, w - 1],
+          [l + 1, w],
+          [l + 1, w + 1],
+        ];
+
+        neighbors.forEach(([l, w]) => {
+          if (
+            l >= 0 &&
+            l < this.box_list.length &&
+            w >= 0 &&
+            w < this.box_list[0].length
+          ) {
+            if (this.box_list[l][w].className == "life") {
+              count++;
+            }
+          }
+        });
+        if (count > 2 && count < 5) {
+          next_row.push(new Box(l, w, this.box_list[l][w].component, "life"));
+        } else {
+          next_row.push(new Box(l, w, this.box_list[l][w].component));
+        }
+      });
+      next_state.push(next_row);
+    });
+
+    this.box_list = next_state;
+  }
+}
--- script.js
+++ script.js
@@ -1,92 +1,129 @@
-import Player from './snake_components/player.js';
-import SnakeGame from './snake_components/snake_game.js';
+import SnakeGame from "./snake_components/snake_game.js";
+import LifeGame from "./life_components/life_game.js";
 
 var globalIntervalContainer = null;
 
-document.getElementById("snek").onclick = function() {
+function teardown_game_state() {
+  clearInterval(globalIntervalContainer);
+  document.getElementById("score") && document.getElementById("score").remove();
+  document.getElementById("new_game_button") &&
+    document.getElementById("new_game_button").remove();
+  document.getElementById("game_field") &&
+    document.getElementById("game_field").remove();
+  document.getElementById("info") && document.getElementById("info").remove();
+  document.getElementById("start_game_button") &&
+    document.getElementById("start_game_button").remove();
+    document.getElementById("restart_game_button") &&
+      document.getElementById("restart_game_button").remove();
+}
 
+document.getElementById("snek").onclick = function () {
   var game;
   document.getElementById("snek").style.backgroundColor = "#7fb3ee";
   document.getElementById("lyfe").style.backgroundColor = "lightblue";
 
   function onSnakeTimerTick(game) {
-    if (game.game_over){
-
-      document.getElementById("info").textContent = "GAME OVER"
-      if (!document.getElementById("new_game_button")){
-        var new_game_button = document.createElement('h1');
+    if (game.game_over) {
+      document.getElementById("info").textContent = "GAME OVER";
+      if (!document.getElementById("new_game_button")) {
+        var new_game_button = document.createElement("h1");
         new_game_button.innerText = "NEW GAME";
         new_game_button.id = "new_game_button";
-        new_game_button.onclick = function() {
+        new_game_button.onclick = function () {
           teardown_game_state();
           setup_game_state();
-        }
+        };
         document.getElementById("info_banner").appendChild(new_game_button);
       }
-
-    }
-    else {
+    } else {
       game.set_game_over(game.player.move());
       game.draw();
     }
   }
-  
+
   function setup_game_state() {
-    var new_game_field = document.createElement('div');
+    var new_game_field = document.createElement("div");
     new_game_field.id = "game_field";
     document.getElementById("game_container").appendChild(new_game_field);
 
-    var new_score_field = document.createElement('p');
+    var new_score_field = document.createElement("p");
     new_score_field.innerText = "Score: 0";
     new_score_field.id = "score";
     document.getElementById("info_banner").appendChild(new_score_field);
 
-    var info_field = document.createElement('p');
+    var info_field = document.createElement("p");
     info_field.id = "info";
     document.getElementById("info_banner").appendChild(info_field);
-  
+
     game = new SnakeGame();
     game.setup();
     globalIntervalContainer = setInterval(() => onSnakeTimerTick(game), 120); // 200 milliseconds = 5 frames per sec
   }
 
-  function teardown_game_state() {
-    clearInterval(globalIntervalContainer);
-    document.getElementById("score") && document.getElementById("score").remove();
-    document.getElementById("new_game_button") && document.getElementById("new_game_button").remove();
-    document.getElementById("game_field") && document.getElementById("game_field").remove();
-    document.getElementById("info") && document.getElementById("info").remove();
+  teardown_game_state();
+  setup_game_state();
+
+  window.addEventListener(
+    "keydown",
+    function (event) {
+      switch (event.code) {
+        case "ArrowLeft":
+          game.player.left();
+          break;
+        case "ArrowRight":
+          game.player.right();
+          break;
+        case "ArrowUp":
+          game.player.up();
+          break;
+        case "ArrowDown":
+          game.player.down();
+          break;
+        default:
+          break;
+      }
+    },
+    true
+  );
+};
+
+document.getElementById("lyfe").onclick = function () {
+  var game;
+
+  function onLifeTimerTick(game) {
+    game.start();
+  }
+
+  function setup_game_state() {
+    document.getElementById("snek").style.backgroundColor = "lightblue";
+    document.getElementById("lyfe").style.backgroundColor = "#7fb3ee";
+
+    var new_game_field = document.createElement("div");
+    new_game_field.id = "game_field";
+    document.getElementById("game_container").appendChild(new_game_field);
+
+    game = new LifeGame();
+    game.setup();
+
+    var start_game_button = document.createElement("h1");
+    start_game_button.innerText = "START GAME";
+    start_game_button.id = "start_game_button";
+    start_game_button.onclick = function () {
+      globalIntervalContainer = setInterval(() => onLifeTimerTick(game), 500);
+      document.getElementById("start_game_button").remove();
+
+      var restart_game_button = document.createElement("h1");
+      restart_game_button.innerText = "RESTART GAME";
+      restart_game_button.id = "restart_game_button";
+      restart_game_button.onclick = function () {
+        teardown_game_state();
+        setup_game_state();
+      };
+      document.getElementById("info_banner").appendChild(restart_game_button);
+    };
+    document.getElementById("info_banner").appendChild(start_game_button);
   }
 
   teardown_game_state();
   setup_game_state();
-
-  window.addEventListener("keydown", function(event) {
-    switch (event.code){
-      case 'ArrowLeft':
-        game.player.left();
-        break;
-      case 'ArrowRight':
-        game.player.right();
-        break;
-      case 'ArrowUp':
-        game.player.up();
-        break;
-      case 'ArrowDown':
-        game.player.down();
-        break;
-      default:
-        break;
-    }
-  }, true);
-}
-
-document.getElementById("lyfe").onclick = function() {
-
-  document.getElementById("snek").style.backgroundColor = "lightblue";
-  document.getElementById("lyfe").style.backgroundColor = "#7fb3ee";
-
-  document.getElementById('game_container').innerHTML = "";
-  document.getElementById('info_banner').innerHTML = "";
-  clearInterval(globalIntervalContainer);
-}
+};
